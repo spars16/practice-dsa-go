03b4be72d0307be862edc7ed243bb36ff42bbe3a
Sparsh Srivastava <sparshsrivastava@sparshs-mbp-2.lan>
2022-01-14 11:11:42 -0500

    updated twosum test

--- algorithms/twosum/two_sum_test.go
+++ algorithms/twosum/two_sum_test.go
@@ -1,8 +1,9 @@
 package twosum
 
 import (
-	"fmt"
 	"testing"
+
+	"github.com/stretchr/testify/assert"
 )
 
 func TestTwoSum(t *testing.T) {
@@ -16,17 +17,17 @@ func TestTwoSum(t *testing.T) {
 			name:       "valid",
 			inputArray: []int{9, 2, 5, 7, 11, 3},
 			target:     7,
-			expected:   []int{5, 2},
+			expected:   []int{1, 2},
 		},
 		{
 			name:       "invalid",
 			inputArray: []int{9, 2, 5, 7, 11, 3},
 			target:     23,
-			expected:   []int{5, 2},
+			expected:   []int{},
 		},
 	}
 	for _, tt := range tests {
 		expected := TwoSum(tt.inputArray, tt.target)
-		fmt.Println(expected)
+		assert.Equal(t, expected, tt.expected)
 	}
 }
